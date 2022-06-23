d00d2ea78e5f67696c0624663bd8ed206b23013b
SquareSheep Studios <dev@dansmith.tech>
2022-06-23 15:21:23 -0400

    Implement contact

--- pages/newsletter/index.js
+++ pages/newsletter/index.js
@@ -9,7 +9,7 @@ import { isEmail } from "../../lib/helpers";
 import sendFetchRequest from "../../lib/fetch";
 
 import newsletterStyles from "../../styles/newsletter.module.css";
-import newsletterSignupStyles from "../../styles/ProductivityTipsSignupStyles.module.css";
+import newsletterSignupStyles from "../../styles/productivityTipsSignup.module.css";
 
 function Newsletter() {
 	const [showModal, setShowModal] = useState(false);
@@ -19,9 +19,13 @@ function Newsletter() {
 		const email = e.target.email.value;
 		if (email.length > 0 && isEmail(email)) {
 			setShowModal(true);
-			sendFetchRequest("/api/newsletter/subscribe?optInStage=1&bevlAnnouncements=true", "POST", {
-				email,
-			});
+			sendFetchRequest(
+				"/api/newsletter/subscribe?optInStage=1&bevlAnnouncements=true",
+				"POST",
+				{
+					email,
+				}
+			);
 			e.target.email.value = null;
 		}
 	}
@@ -36,22 +40,48 @@ function Newsletter() {
 					newsletterStyles["container-full-content"]
 				}
 			>
-				<div className={"container-partial " + newsletterSignupStyles["container-partial"]}>
+				<div
+					className={
+						"container-partial " +
+						newsletterSignupStyles["container-partial"]
+					}
+				>
 					<div className={newsletterSignupStyles["container-info"]}>
-						<h2 className={newsletterSignupStyles["container-info-heading"]}>
+						<h2
+							className={
+								newsletterSignupStyles["container-info-heading"]
+							}
+						>
 							Get Special Offers, and Stay In The Loop
 						</h2>
-						<p className={newsletterSignupStyles["container-info-summary"]}>
-							Bevl's <em>Announcements </em> newsletter reveals sneak previews of the newest features coming
-							to Bevl, and offers exclusive promotions.
+						<p
+							className={
+								newsletterSignupStyles["container-info-summary"]
+							}
+						>
+							Bevl's <em>Announcements </em> newsletter reveals sneak
+							previews of the newest features coming to Bevl, and offers
+							exclusive promotions.
 						</p>
 					</div>
-					<form onSubmit={handleSubmit} className={newsletterSignupStyles["container-form"]}>
-						<div className={newsletterSignupStyles["container-form-input"]}>
-							<FormInput type={"text"} name={"email"} placeholder="Enter email..." required={"true"} />
+					<form
+						onSubmit={handleSubmit}
+						className={newsletterSignupStyles["container-form"]}
+					>
+						<div
+							className={newsletterSignupStyles["container-form-input"]}
+						>
+							<FormInput
+								type={"text"}
+								name={"email"}
+								placeholder="Enter email..."
+								required={"true"}
+							/>
 							<small>No spam. Unsubscribe at any time.</small>
 						</div>
-						<div className={newsletterSignupStyles["container-form-button"]}>
+						<div
+							className={newsletterSignupStyles["container-form-button"]}
+						>
 							<FormButton type={"submit"} value="Sign Up" light={true} />
 							<Modal
 								show={showModal}
