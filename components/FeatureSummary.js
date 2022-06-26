51df3a1809c52225b0cda97c7c644cf690869de1
SquareSheep Studios <dev@dansmith.tech>
2022-06-26 17:02:45 +0100

    Update FeatureSummary.js

--- components/FeatureSummary.js
+++ components/FeatureSummary.js
@@ -76,6 +76,7 @@ export default function FeatureSummary({
 				layout="fill"
 				className={featureSummaryStyles["content-image"]}
 				draggable="false"
+				quality={50}
 				loading={"eager"}
 				blurDataURL={
 					"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP85xNfDwAGIwIqqYhd4AAAAABJRU5ErkJggg=="
