c7cb6583f57bbae9a4d8d17ea6fbe7e2ff2c0a62
SquareSheep Studios <dev@dansmith.tech>
2023-05-24 22:46:43 +0100

    Initial implementation of graphics intro animations

--- components/graphics/Tasks.js
+++ components/graphics/Tasks.js
@@ -1,64 +1,98 @@
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
 import graphicStyles from "../../styles/partials/graphics.module.css";
 
 export default function TasksGraphic() {
+	const intersectionDetectorRef = useRef();
+	const [intersection, setIntersection] = useState(false);
+
+	const getVisibility = () => {
+		if (!intersection) return graphicStyles["visibility"];
+		else
+			return (
+				graphicStyles["visibility"] +
+				" " +
+				graphicStyles["visibility-active"]
+			);
+	};
+
+	useEffect(() => {
+		// watch element to play animation when it loads into the viewport
+		const observer = new IntersectionObserver((entries) => {
+			const entry = entries[0];
+			if (!intersection && entry.isIntersecting) {
+				setIntersection(true);
+				observer.disconnect();
+			}
+		});
+		observer.observe(intersectionDetectorRef.current);
+
+		return () => observer.disconnect();
+	}, []);
+
 	return (
-		<>
+		<div className={getVisibility()} ref={intersectionDetectorRef}>
 			<div
 				id={graphicStyles["container-tasks-container"]}
-				className={graphicStyles["container-image"]}
+				className={graphicStyles["visibility-container"]}
 			>
-				<Image
-					src={"/images/graphics/showcases/tasks/tasks-container.png"}
-					alt={"Tasks Container"}
-					layout="fill"
-					className={graphicStyles["image"]}
-					draggable="false"
-					quality={50}
-					loading={"eager"}
-					blurDataURL={
-						"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP85xNfDwAGIwIqqYhd4AAAAABJRU5ErkJggg=="
-					}
-					placeholder={"blur"}
-				/>
+				<div className={graphicStyles["container-image"]}>
+					<Image
+						src={"/images/graphics/showcases/tasks/tasks-container.png"}
+						alt={"Tasks Container"}
+						layout="fill"
+						className={graphicStyles["image"]}
+						draggable="false"
+						quality={50}
+						loading={"eager"}
+						blurDataURL={
+							"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP85xNfDwAGIwIqqYhd4AAAAABJRU5ErkJggg=="
+						}
+						placeholder={"blur"}
+					/>
+				</div>
 			</div>
 			<div
 				id={graphicStyles["container-day"]}
-				className={graphicStyles["container-image"]}
+				className={graphicStyles["visibility-container"]}
 			>
-				<Image
-					src={"/images/graphics/showcases/tasks/day.png"}
-					alt={"Day"}
-					layout="fill"
-					className={graphicStyles["image"]}
-					draggable="false"
-					quality={50}
-					loading={"eager"}
-					blurDataURL={
-						"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP85xNfDwAGIwIqqYhd4AAAAABJRU5ErkJggg=="
-					}
-					placeholder={"blur"}
-				/>
+				<div className={graphicStyles["container-image"]}>
+					<Image
+						src={"/images/graphics/showcases/tasks/day.png"}
+						alt={"Day"}
+						layout="fill"
+						className={graphicStyles["image"]}
+						draggable="false"
+						quality={50}
+						loading={"eager"}
+						blurDataURL={
+							"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP85xNfDwAGIwIqqYhd4AAAAABJRU5ErkJggg=="
+						}
+						placeholder={"blur"}
+					/>
+				</div>
 			</div>
 			<div
 				id={graphicStyles["container-task-drag"]}
-				className={graphicStyles["container-image"]}
+				className={graphicStyles["visibility-container"]}
 			>
-				<Image
-					src={"/images/graphics/showcases/tasks/task-drag.png"}
-					alt={"Task Drag"}
-					layout="fill"
-					className={graphicStyles["image"]}
-					draggable="false"
-					quality={50}
-					loading={"eager"}
-					blurDataURL={
-						"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP85xNfDwAGIwIqqYhd4AAAAABJRU5ErkJggg=="
-					}
-					placeholder={"blur"}
-				/>
+				<div className={graphicStyles["container-image"]}>
+					<Image
+						src={"/images/graphics/showcases/tasks/task-drag.png"}
+						alt={"Task Drag"}
+						layout="fill"
+						className={graphicStyles["image"]}
+						draggable="false"
+						quality={50}
+						loading={"eager"}
+						blurDataURL={
+							"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP85xNfDwAGIwIqqYhd4AAAAABJRU5ErkJggg=="
+						}
+						placeholder={"blur"}
+					/>
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
